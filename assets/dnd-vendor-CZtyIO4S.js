0849a171e947a790979a02436a51b09c5b15dc39
HyperWolf051 <HyperWolf051@users.noreply.github.com>
2025-10-29 11:33:12 +0000

    deploy: f2b5251c535dc522011195d4d4e4afa707be8311

---
+++ assets/dnd-vendor-CZtyIO4S.js
@@ -0,0 +1,1 @@
+import"./react-vendor-DD48japz.js";
---
+++ assets/validation-Bmklxit8.js
@@ -0,0 +1,1 @@
+const t=t=>{if(!t)return!1;return/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(t)},s=t=>null!=t&&("string"!=typeof t||t.trim().length>0);export{t as a,s as v};
